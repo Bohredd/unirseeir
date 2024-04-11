c085f03bc92a8cc382f796924e8d0139bbf59bf7
Diogo <dapereira@inf.ufsm.br>
2024-04-11 09:47:51 -0300

    feat: UNIr-se e Ir

--- core/templatetags/caronas_tags.py
+++ core/templatetags/caronas_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from geopy.distance import geodesic
 from geopy.geocoders import Nominatim
+import folium
+from geopy.exc import GeocoderUnavailable
 
 register = template.Library()
 
@@ -43,3 +45,36 @@ def get_distancia_saida(latitude, longitude, carona):
 
     print("Menor distancia: ", menor_distancia)
     return float(menor_distancia)
+
+def get_coordinates(address):
+    geolocator = Nominatim(user_agent="my_app")
+    try:
+        location = geolocator.geocode(address)
+        if location:
+            return (location.latitude, location.longitude)
+        else:
+            return None
+    except GeocoderUnavailable:
+        return None
+
+@register.simple_tag
+def gerar_mapa(deslocamento):
+    endereco_saida = deslocamento.ponto_saida_endereco
+    endereco_destino = deslocamento.ponto_destino_endereco
+
+    coordenadas_saida = get_coordinates(endereco_saida)
+    coordenadas_destino = get_coordinates(endereco_destino)
+
+    if coordenadas_saida and coordenadas_destino:
+        mapa = folium.Map(location=[coordenadas_saida[0], coordenadas_saida[1]], zoom_start=15)
+
+        folium.Marker([coordenadas_saida[0], coordenadas_saida[1]], popup='Ponto de Saída').add_to(mapa)
+        folium.Marker([coordenadas_destino[0], coordenadas_destino[1]], popup='Ponto de Destino').add_to(mapa)
+
+        folium.PolyLine(locations=[(coordenadas_saida[0], coordenadas_saida[1]), (coordenadas_destino[0], coordenadas_destino[1])], color='blue').add_to(mapa)
+
+        mapa_html = mapa._repr_html_()
+
+        return mapa_html
+    else:
+        return "Não foi possível obter as coordenadas para o deslocamento."
